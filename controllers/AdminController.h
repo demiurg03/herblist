798712e80b3b8489315d15f85be3d5fb0283b755
Максим <palshin.maxim.alekseevich@gmail.com>
2022-05-29 11:35:56 +0700

    update adminController

--- controllers/AdminController.h
+++ controllers/AdminController.h
@@ -6,16 +6,6 @@
 
 #include <drogon/drogon.h>
 
-#include <fmt/format.h>
-#include <fmt/ranges.h>
-
-
-#include <libzippp/libzippp.h>
-
-#include <nlohmann/json.hpp>
-
-
-
 #include "const.h"
 
 
@@ -23,37 +13,6 @@
 
 
 
-
-
-
-void write(const std::string &data, const std::string &file);
-
-
-struct Herb{
-    std::string name;
-    std::map<std::string,std::string> tr_name;
-    std::map<std::string,std::string> tr_content_path;
-};
-
-class FileRemover{
-public:
-
-    FileRemover(const std::string_view path) : _path(path){
-
-    }
-
-    ~FileRemover(){
-        namespace fs = std::filesystem;
-
-        if( fs::exists( _path ) ){
-            fs::remove( _path );
-        }
-
-    }
-
-private:
-    std::string _path;
-};
 
 
 class AdminController : public drogon::HttpController<AdminController>
@@ -67,7 +26,8 @@ public:
     PATH_LIST_BEGIN
 
     ADD_METHOD_TO(AdminController::render,"admin/", drogon::Get);
-    ADD_METHOD_TO(AdminController::upload,"admin/upload", drogon::Post);
+    ADD_METHOD_TO(AdminController::upload,"admin/upload", drogon::Get);
+    ADD_METHOD_TO(AdminController::addHerb,"admin/addHerb", drogon::Post);
 
     PATH_LIST_END
 
@@ -76,8 +36,8 @@ public:
 
     void upload(const drogon::HttpRequestPtr &req,  std::function<void(const drogon::HttpResponsePtr &)> &&callback);
 
+    void addHerb(const drogon::HttpRequestPtr &req,  std::function<void(const drogon::HttpResponsePtr &)> &&callback);
 
-    bool fileWork(const std::string &stringPath);
 
 
     bool isCorrectAddress(const drogon::HttpRequestPtr &req);
